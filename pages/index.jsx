96e4f9866e978b0dfdc3b13dfe6a340716119f20
Joshhortt <joshhortt@yahoo.com>
2024-05-11 13:11:23 +0100

    fix images: show testemonial image

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,8 +11,7 @@ export default function Home({ page }) {
 					<blockquote className="testimonial" key={i}>
 						<p className="testimonial-message">{testimonial.message}</p>
 						<p className="testimonial-author">
-							{/* <img src={testimonial.testimonial_image} alt={`Photo of ${testimonial.name}`} />  */}
-							{testimonial.name}
+							<img src={testimonial.testimonial_image} alt={`Photo of ${testimonial.name}`} /> 
 						</p>
 					</blockquote>
 				))}
